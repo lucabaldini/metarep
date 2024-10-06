4345ccd385038718b5bd5917a1970828490ac7b1
tmssriss <t.risso@studenti.unipi.it>
2024-10-06 16:10:52 +0200

    added an integration feature

--- pdf.py
+++ pdf.py
@@ -19,6 +19,9 @@ class ProbabilityDensityDistribution(InterpolatedUnivariateSpline):
 
     def normalization(self):
         return self.integral(self._x.min(), self._x.max())
+    
+    def integrate(self, x1, x2):
+        return self.integral(x1, x2)
 
 
 
@@ -27,8 +30,15 @@ if __name__ == '__main__':
     x = np.linspace(0., 1., 4)
     y = np.exp(x)
     pdf = ProbabilityDensityDistribution(x, y)
+    
     x0 = 0.5
-    print(np.exp(x0), pdf(x0))
+    print(np.exp(x0), pdf(x0)) #InterpolateUnivariateSpline has a __call__ method
     print(pdf.normalization())
+
+    #UT: evaluate the probability of the random variable to be on a given interval
+    x1 = 0
+    x2 = 1
+    print(pdf.integrate(x1, x2))
+
     pdf.plot()
     plt.show()
